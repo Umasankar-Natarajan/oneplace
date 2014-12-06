c4f73d34c97408eb1fdc3677a5902a784cf0b31d
Dinesh Pulivalam Ramakrishnan <pr.dinesh@live.in>
2014-12-06 14:07:26 +0530

    Adding Onedrive

--- php/application/controllers/home.php
+++ php/application/controllers/home.php
@@ -1,18 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-
+  public function __construct()
+  {
+    parent::__construct();
+      $this->load->library('session');
+      $this->load->helper('url');
+  }
+  
+  
 	public function index()
 	{
 		$this->load->view('home');
 	}
   
-      public function __construct()
-    {
-    	parent::__construct();
-        $this->load->library('session');
-        $this->load->helper('url');
-    }
     // Call this method first by visiting http://SITE_URL/example/request_dropbox
     public function request_dropbox()
 	{
